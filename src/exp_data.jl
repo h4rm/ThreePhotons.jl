baeb04270c858c90ad27f9cde51ba724d523a9c6
Benjamin von Ardenne <benjamin.von.ardenne@gmail.com>
2017-08-18 11:24:37 +0200

    Optimized exp data analysis

--- src/exp_data.jl
+++ src/exp_data.jl
@@ -59,30 +59,38 @@ function calculate_correlations_in_image(image_list::Array{Array{Float64,2},1}, 
 
     distances,angles = precompute_distances_and_angles(sx, N)
 
-    c2_full,c3_full = @sync @parallel ( (a,b) -> (a[1]+b[1], a[2]+b[2])) for i=1:length(image_list)
-        image = image_list[i]
-        println("Processing image #$(i)")
-        c2_local = zeros(Float64, N, K2, K2)
-        c3_local = zeros(Float64, N, N, K3, K3, K3)
-        for x1 = range
-            for y1 = range
-                @inbounds k1 = distances[x1,y1]
+    c2_full = zeros(Float64, N, K2, K2)
+    c3_full = zeros(Float64, N, N, K3, K3, K3)
+    number_analyzed_images = 0
 
-                for x2 = range
-                    for y2 = range
-                        @inbounds k2 = distances[x2,y2]
+    for j=1:ceil(Int64, length(image_list)/nworkers())
+        println("Processing batch $j")
+        #Processing next batch of nworkers() images
+        c2_part,c3_part = @sync @parallel ( (a,b) -> (a[1]+b[1], a[2]+b[2])) for i=((j-1)*nworkers()+1):clamp(j*nworkers()+1, 1, length(image_list))
+            image = image_list[i]
+            println("Processing image #$(i)")
+            c2_local = zeros(Float64, N, K2, K2)
+            c3_local = zeros(Float64, N, N, K3, K3, K3)
+            for x1 = range
+                for y1 = range
+                    @inbounds k1 = distances[x1,y1]
 
-                        if k1 >= k2 && k1 > 0 && k2 > 0 && k1 <= K2 && k2 <= K2
-                            @inbounds ai = angles[x1,y1,x2,y2]
-                            @inbounds c2_local[ai,k1,k2] += real(image[x1,y1]*image[x2,y2]) * doubletFactor(k1,k2) * 1/(k1*k2)
+                    for x2 = range
+                        for y2 = range
+                            @inbounds k2 = distances[x2,y2]
 
-                            for x3 = range
-                                for y3 = range
-                                    @inbounds k3 = distances[x3,y3]
+                            if k1 >= k2 && k1 > 0 && k2 > 0 && k1 <= K2 && k2 <= K2
+                                @inbounds ai = angles[x1,y1,x2,y2]
+                                @inbounds c2_local[ai,k1,k2] += real(image[x1,y1]*image[x2,y2]) * doubletFactor(k1,k2) * 1/(k1*k2)
 
-                                    if k2 >= k3 && k3 > 0 && k1 <= K3 && k2<= K3 && k3 <= K3
-                                        bi = angles[x1,y1,x3,y3]
-                                        @inbounds c3_local[ai,bi,k1,k2,k3] += real(image[x1,y1]*image[x2,y2]*image[x3,y3]) * tripletFactor(k1,k2,k3) * 1/(k1*k2*k3)
+                                for x3 = range
+                                    for y3 = range
+                                        @inbounds k3 = distances[x3,y3]
+
+                                        if k2 >= k3 && k3 > 0 && k1 <= K3 && k2<= K3 && k3 <= K3
+                                            bi = angles[x1,y1,x3,y3]
+                                            @inbounds c3_local[ai,bi,k1,k2,k3] += real(image[x1,y1]*image[x2,y2]*image[x3,y3]) * tripletFactor(k1,k2,k3) * 1/(k1*k2*k3)
+                                        end
                                     end
                                 end
                             end
@@ -90,9 +98,14 @@ function calculate_correlations_in_image(image_list::Array{Array{Float64,2},1}, 
                     end
                 end
             end
+            flush(STDOUT)
+            (c2_local, c3_local)
         end
+        c2_full += c2_part
+        c3_full += c3_part
+        number_analyzed_images += nworkers()
+
+        serializeToFile(filename, ( Dict("num_pictures"=>number_analyzed_images, "num_incident_photons"=>0, "qcut"=>1.0, "K2"=>K2, "K3"=>K3, "N"=>N, "dq"=>0.1), c2_full, c3_full))
         flush(STDOUT)
-        (c2_local, c3_local)
     end
-    serializeToFile(filename, ( Dict("num_pictures"=>0, "num_incident_photons"=>0, "qcut"=>1.0, "K2"=>K2, "K3"=>K3, "N"=>N, "dq"=>0.1), c2_full, c3_full))
 end
