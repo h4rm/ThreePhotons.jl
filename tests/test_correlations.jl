862219aabb8ad70f56b5d3952565b4bfe558cbcc
Benjamin von Ardenne <benjamin.von.ardenne@gmail.com>
2017-09-05 10:33:36 +0200

    Trying out fix for pinv tolerance

--- tests/test_correlations.jl
+++ tests/test_correlations.jl
@@ -6,7 +6,7 @@ basis = calculate_basis(L,LMAX,N,K)
 println("Calculate two photon correlation.")
 c2 = twoPhotons(intensity, basis, K)
 #dummy reference c3
-c3ref = zeros(N,N,K,K,K)
+c3ref = zeros(N,2*N,K,K,K)
 println("calculate energy.")
 #energy calculation including three-photon correlation
 energy(intensity, basis, c3ref)
