f4047a0f97ae9a35a18b160ea15e63d6959c4e3f
Benjamin von Ardenne <benjamin.von.ardenne@gmail.com>
2017-11-02 11:22:39 +0100

    Crambin beamstop

--- jobs/run_generate_histograms.jl
+++ jobs/run_generate_histograms.jl
@@ -129,6 +129,32 @@ function run_calculate_beamstop_correlation(jobname::String, images_path::String
     launch_job("exp_data/$(jobname)", Ncores, false, julia_script, 1)
 end
 
+"""Distributes the calculation of correlations among many jobs"""
+function run_calculate_generic_beamstop_correlation(jobname::String, K2::Int64, K3::Int64, N::Int64; Ncores::Int64=8)
+
+    julia_script = """
+    using ThreePhotons
+    using Images
+
+    K2 = $K2
+    K3 = $K3
+    N = $N
+
+    function beamstop(K::Vector{Float64}, beamstop_width::Float64)
+        abs(K[1]) > beamstop_width && abs(K[2]) > beamstop_width ? 1.0 : 0.0
+    end
+
+    qm = 3.141592653589793
+    delq = 0.08267349088394192
+    range = -qm:delq:qm
+    # range = linspace(-qm,qm,2*intensity_K38.KMAX)
+    crambin_beamstop = [beamstop([k1,k2], 0.1) == 1.0 ? 0.0 : 1.0 for k1 in range, k2 in range]
+
+    calculate_correlations_in_image([crambin_beamstop], K2, K3, N)
+    """
+    launch_job("exp_data/$(jobname)", Ncores, false, julia_script, 1)
+end
+
 exp_filelist = String["amo86615_186_PR772_single.h5",
 "amo86615_188_PR772_single.h5",
 "amo86615_190_PR772_single.h5",
@@ -146,6 +172,9 @@ exp_filelist = String["amo86615_186_PR772_single.h5",
 
 #Calculate beamstop of Coliphage_PR772
 # run_calculate_beamstop_correlation("coliphage_beamstop", environment_path("exp_data/Coliphage_PR772/amo86615_186_PR772_single.h5"), 38, 26, 32)
+
+#Calculate beamstop of crambin
+# run_calculate_generic_beamstop_correlation("crambin_beamstop", 38, 26, 32)
 
 function combine_histograms(dir::String, num::Int64)
     println("Processing $dir")
