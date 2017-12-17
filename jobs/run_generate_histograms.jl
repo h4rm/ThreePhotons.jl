86f2ec66c0827fccdc1b1f48150e513e3b902c2f
Benjamin von Ardenne <benjamin.von.ardenne@gmail.com>
2017-12-17 15:10:50 +0100

    New histograms, yeah

--- jobs/run_generate_histograms.jl
+++ jobs/run_generate_histograms.jl
@@ -1,5 +1,5 @@
 function histogram_name(prefix::String, ppi::Int64, N::Int64, K2::Int64, K3::Int64, rmax::Float64, max_pictures::Int64, suffix::String, gamma::Float64=0.0, sigma::Float64=1.0, lambda::Float64=0.0)
-    return "$(prefix)$(ppi)p_N$(N)_K2_$(K2)_K3_$(K3)_R$(rmax)_P$(max_pictures)$(gamma > 0.0 ? "_G$(gamma)_S$(sigma)" : "")_lambda$(lambda)_$(suffix)"
+    return "$(prefix)$(ppi)p_N$(N)_K2_$(K2)_K3_$(K3)_R$(rmax)_P$(max_pictures)$(gamma > 0.0 ? "_G$(gamma)_S$(sigma)" : "")_lambda_$(lambda)_$(suffix)"
 end
 
 """Starts a cluster job for synthetic correlation generation"""
