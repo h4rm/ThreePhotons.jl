2c4bb4ec9e2c060c38ffa60a815b7f0e4ed536e1
Benjamin von Ardenne <benjamin.von.ardenne@gmail.com>
2017-12-19 13:05:19 +0100

    GPU memory

--- jobs/environment_owl.jl
+++ jobs/environment_owl.jl
@@ -32,10 +32,10 @@ function jobengine_head(name::String, dir::String, Ncores::Integer, gpu::Bool; h
 end
 
 """Launches the script via qsub, in case dir doesnt exist, create it"""
-function launch_job(dir::String, Ncores::Integer, gpu::Bool, julia_script::String, successive_jobs::Integer=1; hours::Int64=48, architecture::String="ivy-bridge|sandy-bridge|haswell|broadwell|skylake", memory::String="", fresh::Bool=false)
+function launch_job(dir::String, Ncores::Integer, gpu::Bool, julia_script::String, successive_jobs::Integer=1; hours::Int64=48, architecture::String="ivy-bridge|sandy-bridge|haswell|broadwell|skylake", memory::String="", fresh::Bool=false, gpu_mem::String="4000M")
     githead = check_git_status()
     full_dir = environment_path(dir)
-    head = jobengine_head(dir, full_dir, Ncores, gpu; hours=hours, architecture=architecture, memory=memory)
+    head = jobengine_head(dir, full_dir, Ncores, gpu; hours=hours, architecture=architecture, memory=memory, gpu_mem=gpu_mem)
 
     if fresh run(`rm -rf $(full_dir)`) end
 
